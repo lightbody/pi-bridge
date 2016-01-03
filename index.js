2467d8607037ef3744e16e841672b95a3efd4e08
Patrick Lightbody <patrick@lightbody.net>
2016-01-02 23:28:42 -0800

    Support for Denon receiver

--- index.js
+++ index.js
@@ -14,16 +14,21 @@ app.post('/', function(req, res) {
   
   var intent = req.body.request.intent.name;
   if (intent == 'Mute') {
-    yamaha("@MAIN:MUTE=On", res);
+    //yamaha("@MAIN:MUTE=On", res);
+    denon("MUON", res);
   } else if (intent == 'UnMute') {
-    yamaha("@MAIN:MUTE=Off", res);
+    //yamaha("@MAIN:MUTE=Off", res);
+    denon("MUOFF", res);
   } else if (intent == 'WatchTV') {
-    yamaha("@MAIN:SCENE=Scene 2", res);
-  } else if (intent = 'WatchAppleTV') {
-    yamaha("@MAIN:SCENE=Scene 1", res);
-  } else if (intent = 'TurnOff') {
-    cec(function() {
-      yamaha("@MAIN:PWR=Standby", res);
+    //yamaha("@MAIN:SCENE=Scene 2", res);
+    denon("SISAT/CBL", res);
+  } else if (intent == 'WatchAppleTV') {
+    //yamaha("@MAIN:SCENE=Scene 1", res);
+    denon("SIMPLAY", res);
+  } else if (intent == 'TurnOff') {
+    cec("standby 0", function() {
+      //yamaha("@MAIN:PWR=Standby", res);
+      denon("PWSTANDBY", res);
     });
   }
 });
@@ -37,8 +42,19 @@ var yamaha = function(cmd, res) {
   });
 }
 
+var denon = function(cmd, res) {
+  var client = net.connect({host: "10.0.1.230", port: 23}, function() { //'connect' listener
+    client.end(cmd + '\r\n', function() {
+        client.destroy();
+        sayOk(res);
+    });        
+  });
+}
+
 var cec = function(cmd, callback) {
-  exec("echo \" + cmd + \" | cec-client -s", function(err, stdout, stderr) {
+  console.log("Running: " + cmd);
+  exec('echo "' + cmd + '" | cec-client -s', function(err, stdout, stderr) {
+    console.log("DONEs");
     console.log(err);
     console.log(stdout);
     console.log(stderr);
